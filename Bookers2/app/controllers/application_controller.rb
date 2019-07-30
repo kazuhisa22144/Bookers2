89fd57892af5f68eaf48e67937284e04919b0e6a
kazuhisa22144 <Mkazuhisa@gmail.com>
2019-07-30 13:46:06 +0900

    Bookers2[2]

--- Bookers2/app/controllers/application_controller.rb
+++ Bookers2/app/controllers/application_controller.rb
@@ -21,3 +21,5 @@
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
+
+Refile.secret_key = '660d0eefe94a2523a3c1cbcd3a6a6aa9fe1dd93d6702f7d45e98ddd3ba9ebf471812afa67fc5f72eb54fa28cfc5783e76dc32162ca27953cc64553d584eae3b5'
